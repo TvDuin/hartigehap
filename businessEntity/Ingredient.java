6b7d7cc8122abcb1bff1de409bbd383ac70caff1
msfranke <msfranke@student.avans.nl>
2015-04-07 14:25:19 +0200

    Ingredient toegevoegd

--- businessEntity/Ingredient.java
+++ businessEntity/Ingredient.java
@@ -4,4 +4,37 @@ package businessEntity;
 
 public class Ingredient {
 
+    private int ingredientID;
+    private String ingredientNaam;
+    private int voorraad;
+    
+    public Ingredient(int ingredientID, String ingredientNaam, int voorraad) {
+            this.ingredientID = ingredientID;
+            this.ingredientNaam = ingredientNaam;
+            this.voorraad = voorraad;
+    }
+
+    public int getIngredientID() {
+        return ingredientID;
+    }
+
+    public String getIngredientNaam() {
+        return ingredientNaam;
+    }
+
+    public int getVoorraad() {
+        return voorraad;
+    }
+
+    public void setIngredientID(int ingredientID) {
+        this.ingredientID = ingredientID;
+    }
+
+    public void setIngredientNaam(String ingredientNaam) {
+        this.ingredientNaam = ingredientNaam;
+    }
+
+    public void setVoorraad(int voorraad) {
+        this.voorraad = voorraad;
+    }
 }
