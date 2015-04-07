f70d00dcc10cbcf643aebab14fa057c22a483be9
idolotsa <gorp46@gmail.com>
2015-04-07 13:16:50 +0200

    dranken aangepast. constructor en getters en setters aangemaakt

--- businessEntity/Drank.java
+++ businessEntity/Drank.java
@@ -1,5 +1,32 @@
 package businessEntity;
-// hoi
+// hoi Hoe gaat ie.
 public class Drank {
+    private int drankId;
+    private String drankNaam;
+    private int voorraad;
+
+    public Drank(int drankId, String drankNaam, int voorraad) {
+        this.drankId = drankId;
+        this.drankNaam = drankNaam;
+        this.voorraad = voorraad;
+    }
+
+    public int getDrankId() {
+        return drankId;
+    }
+
+    public String getDrankNaam() {
+        return drankNaam;
+    }
+
+    public int getVoorraad() {
+        return voorraad;
+    }
+
+    public void setVoorraad(int voorraad) {
+        this.voorraad = voorraad;
+    }
+    
+    
   
 }
