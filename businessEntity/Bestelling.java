c0da4878dbaf290787370473e00b4e8ace30063e
tvduin <t.vanduin1@student.avans.nl>
2015-04-07 13:17:44 +0200

    bestelling class updated

--- businessEntity/Bestelling.java
+++ businessEntity/Bestelling.java
@@ -4,4 +4,39 @@ package businessEntity;
 
 public class Bestelling {
 
+  public int id;
+  public int tafelId;
+  public ArrayList<Gerecht> gerechten;
+  public ArrayList<Drank> dranken;
+
+  public Bestelling(int id, int tafelId, ArrayList<Gerecht> gerechten, ArrayList<Drank> dranken) {
+    this.id = id;
+    this.tafelId = tafelId;
+    this.gerechten = gerechten;
+    this.dranken = dranken;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getTafelId() {
+    return tafelId;
+  }
+
+  public void set(int tafelId) {
+    this.tafelId = tafelId;
+  }
+
+  public ArrayList<Gerecht> getGerechten() {
+    return gerechten;
+  }
+
+  public ArrayList<Drank> getDranken() {
+    return dranken;
+  }
 }
