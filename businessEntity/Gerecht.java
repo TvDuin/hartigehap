514fcac36faade2edab2e7d53533a08ba8f07330
wpdmaul <wpd.maul@gmail.com>
2015-04-07 16:05:58 +0200

    gerecht afgemaakt

--- businessEntity/Gerecht.java
+++ businessEntity/Gerecht.java
@@ -2,21 +2,28 @@ package businessEntity;
 
 //Contains all information about every dish
 
+import java.util.ArrayList;
+
+
 public class Gerecht {
     
     private String gerechtGegevens;
     private String gerechtNaam;
     private double gerechtPrijs;
     private int gerechtID;
- 
+    private ArrayList<Ingredient> ingredienten;
     
     public Gerecht(int gerechtID, String gerechtNaam, String gerechtGegevens, double gerechtPrijs)
     {
-        this.gerechtGegevens = gerechtGegevens;
+        this.gerechtID = gerechtID;
         this.gerechtNaam = gerechtNaam;
-        this.gerechtPrijs = gerechtPrijs;     
+        this.gerechtGegevens = gerechtGegevens;
+        this.gerechtPrijs = gerechtPrijs;
     }
-    
+    public  int getGerechtID()
+   {
+       return gerechtID;
+   }
     public String getGerechtNaam()
     {
         return gerechtNaam;
@@ -26,12 +33,37 @@ public class Gerecht {
    {
        return gerechtGegevens;
    }
-   public  int getGerechtID()
-   {
-       return gerechtID;
-   }
+   
    public double getGerechtPrijs()
    {
        return gerechtPrijs;
-}
+    }
+   public ArrayList<Ingredient> getIngredienten()
+   {
+       return ingredienten;
+   }
+   
+   
+   public void setGerechtID(int gerechtID)
+   {
+       this.gerechtID = gerechtID;
+   }
+   public void setGerechtNaam(String gerechtNaam)
+   {
+       this.gerechtNaam = gerechtNaam;
+   }
+   public void setGerechtGegevens(String gerechtGegevens)
+   {
+       this.gerechtGegevens = gerechtGegevens;
+   }
+   public void setGerechtPrijs(double gerechtPrijs)
+   {
+       this.gerechtPrijs = gerechtPrijs;
+   }
+  
+   public void addIngredient(Ingredient ingredient)
+   {
+       ingredienten.add(ingredient);
+   }
+
 }
