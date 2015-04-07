fecd34413245c0b2e134051dca265352621ff648
wpdmaul <wpd.maul@gmail.com>
2015-04-07 14:24:22 +0200

    blade

--- businessEntity/Gerecht.java
+++ businessEntity/Gerecht.java
@@ -3,5 +3,35 @@ package businessEntity;
 //Contains all information about every dish
 
 public class Gerecht {
-
+    
+    private String gerechtGegevens;
+    private String gerechtNaam;
+    private double gerechtPrijs;
+    private int gerechtID;
+ 
+    
+    public Gerecht(int gerechtID, String gerechtNaam, String gerechtGegevens, double gerechtPrijs)
+    {
+        this.gerechtGegevens = gerechtGegevens;
+        this.gerechtNaam = gerechtNaam;
+        this.gerechtPrijs = gerechtPrijs;     
+    }
+    
+    public String getGerechtNaam()
+    {
+        return gerechtNaam;
+    }
+    
+   public String getGerechtGegevens()
+   {
+       return gerechtGegevens;
+   }
+   public  int getGerechtID()
+   {
+       return gerechtID;
+   }
+   public double getGerechtPrijs()
+   {
+       return gerechtPrijs;
+}
 }
